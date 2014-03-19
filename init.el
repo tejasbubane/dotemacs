d02c19ed3786c3ac6ca18740a4d48ccfc5281858
Tejas Bubane <tejas.bubane@infibeam.net>
2014-03-19 11:52:38 +0530

    First commit : Very basic rails setup

---
+++ init.el
@@ -0,0 +1,86 @@
+;; Initialization stuff
+(add-to-list 'load-path "~/.emacs.d/")
+(setq root-dir (file-name-directory
+	(or (buffer-file-name) load-file-name)))
+(setq inhibit-startup-screen t) ; don't show the startup screen
+(global-auto-revert-mode 1)     ; auto revert buffers when changed on disk
+(show-paren-mode t)             ; visualize()
+(iswitchb-mode t)               ; use advanced tab switching
+(blink-cursor-mode -1)          ; no cursor blinking
+(tool-bar-mode -1)              ; disable the awful toolbar
+(menu-bar-mode -1)              ; no menu bar
+(scroll-bar-mode -1)            ; disable the sroll bar
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Package Manager
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+;; Pallet keeps Cask file in sync with packages installed by package-install
+(require 'pallet)
+
+;; Theme and visual stuff
+(add-to-list 'load-path "~/.emacs.d/modules")
+(require 'color-theme-railscasts)
+(color-theme-railscasts)
+(set-frame-font "Consolas 10" nil t)
+(setq echo-keystrokes 0.02)
+;; Powerline
+(require 'powerline)
+
+;; Magit
+(require 'magit)
+(eval-after-load 'magit
+  (progn '(global-set-key (kbd "C-x g") 'magit-status)))
+
+;; Enhanced Ruby Mode
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(setq enh-ruby-bounce-deep-indent t)
+(setq enh-ruby-hanging-brace-indent-level 2)
+
+;; Auto-complete mode
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/.cask/24.3.1/elpa/auto-complete-20140208.653/dict")
+(ac-config-default)
+(setq ac-ignore-case nil)
+(add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
+(add-to-list 'ac-modes 'python-mode)
+
+;; Projectile mode
+(require 'grizzl)
+(projectile-global-mode +1)
+(setq projectile-indexing-method 'git)
+(setq projectile-completion-system 'grizzl)
+;; Press Command-p for fuzzy find in project
+(global-set-key (kbd "s-p") 'projectile-find-file)
+;; Press Command-b for fuzzy switch buffer
+(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+
+;;(require 'flx-ido)
+;;(ido-mode 1)
+;;(ido-everywhere 1)
+;;(flx-ido-mode 1)
+;;(setq ido-use-faces nil) ;; disable ido faces to see flx highlights.
+
+;; Highlight-indentation
+(require 'highlight-indentation)
+(add-hook 'enh-ruby-mode-hook
+	  (lambda () (highlight-indentation-current-column-mode)))
+
+;; Flyspell
+(require 'flyspell)
+(setq flyspell-issue-message-flg nil)
+(add-hook 'enh-ruby-mode-hook
+          (lambda () (flyspell-prog-mode)))
+(ac-flyspell-workaround)
+
+;; Multiple Cursors
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
