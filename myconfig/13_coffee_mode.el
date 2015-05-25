28bbf2dfce528404a615677b00610078c6317626
Tejas Bubane <tejasbubane@gmail.com>
2015-05-26 00:03:52 +0530

    Remove coffee-mode.

--- myconfig/13_coffee_mode.el
+++
@@ -1,8 +0,0 @@
-;; automatically clean up bad whitespace
-(setq whitespace-action '(auto-cleanup))
-
-;; only show bad whitespace
-(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab))
-
-;; This gives you a tab of 2 spaces
-(custom-set-variables '(coffee-tab-width 2))
