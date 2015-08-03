64052480c74bff5ea2e64caae082303282f33130
Tejas Bubane <tejasbubane@gmail.com>
2015-08-03 13:52:36 +0530

    Add exercism.el to vendor and load all files from vendor directory.

--- init.el
+++ init.el
@@ -7,13 +7,14 @@
 
 ;; Load all config files
 (setq root-dir "~/.emacs.d")
+
 (setq myconfig (concat root-dir "/myconfig"))
 (add-to-list 'load-path myconfig)
 
-;; load all files from myconfig directory
-(add-to-list 'load-path "~/.emacs.d/vendor") ;; third party plugins
+(setq vendor (concat root-dir "/vendor"))
+(add-to-list 'load-path vendor) ;; third party plugins
 
 (mapcar 'load (directory-files myconfig t "\\.el\\'"))
-
+(mapcar 'load (directory-files vendor t "\\.el\\'"))
 
 ;;; init.el ends here
