125c930be3712a131a8cf1f808394f43e7545788
Tejas Bubane <tejasbubane@gmail.com>
2015-12-30 12:09:43 +0530

    JSX and coffeescript modes highlighting and indentation
    Using web-mode for jsx files instead of jsx-mode since it does
    not index the jsx(html) properly

---
+++ myconfig/27_jsx_mode.el
@@ -0,0 +1,7 @@
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+(setq web-mode-code-indent-offset 2)
