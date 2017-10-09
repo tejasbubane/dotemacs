67a1cd45f3671ea325e6fdf1b19c6bf098bbd48d
Tejas Bubane <tejasbubane@gmail.com>
2017-10-09 17:14:22 +0530

    Fix projectile-config missing
    Remove * glob ignore

---
+++ config/projectile-config.el
@@ -0,0 +1,13 @@
+(use-package projectile
+  :init
+  (use-package projectile-rails
+    :config
+    (add-hook 'projectile-mode-hook 'projectile-rails-on)
+    (setq projectile-rails-add-keywords nil))
+
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t)
+  (setq projectile-tags-command "ctags-exuberant -Re --languages=ruby --exclude=.git --exclude=log %s %s ."))
+
+(provide 'projectile-config)
