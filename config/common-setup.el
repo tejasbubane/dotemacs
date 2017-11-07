008f44823ac86bf1614e168bac993337ff3ba444
Tejas Bubane <tejasbubane@gmail.com>
2017-11-07 15:05:57 +0530

    Enable subword mode by default
    Ref: http://wikemacs.org/wiki/Subword-mode
    
    Seems useful for kill-word especially with camelCase words being
    treated separate.

--- config/common-setup.el
+++ config/common-setup.el
@@ -67,4 +67,8 @@
 ;; https://github.com/jwiegley/use-package#diminishing-and-delighting-minor-modes
 (use-package diminish)
 
+;; Use subword mode - treats parts of camelCase words separate (camel, Case)
+;; Useful for kill-word
+(global-subword-mode +1)
+
 (provide 'common-setup)
