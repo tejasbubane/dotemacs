87f444dc3a85169437e1a012b0130c6d84c55f90
Tejas Bubane <tejasbubane@gmail.com>
2017-11-09 01:01:26 +0530

    Configure emacs to autoformat using prettier before save
    For js2-mode. Since rjsx mode derives from js2-mode, the hook will run
    there as well.

--- config/js2-config.el
+++ config/js2-config.el
@@ -1,19 +1,20 @@
 (use-package js2-mode
   :mode ("\\.js\\'")
 
+  :init
+  (use-package prettier-js)
+  (use-package add-node-modules-path)
+
   :config
   (defun my-js2-mode-hook()
     ;; Fix offset to 2 (default is 4)
     (setq js2-basic-offset 2)
-
-    ;; disable jshint since we prefer eslint checking
+    ;; disable jshint
     (setq-default flycheck-disabled-checkers
                   '(javascript-jshint jsxhint-checker json-jsonlint))
-
-    (eval-after-load 'flycheck
-      '(flycheck-add-mode 'javascript-eslint 'js2-mode))
+    (add-node-modules-path)
     (flycheck-mode 1)
-
+    (prettier-js-mode)
     (setq-default
      js2-ignored-warnings
      '("msg.no.side.effects" "msg.missing.semi")))
--- config/rjsx-config.el
+++ config/rjsx-config.el
@@ -1,17 +1,12 @@
 (use-package rjsx-mode
   :mode "components\\/.*\\.js\\'"
 
-  :init
-  (use-package prettier-js)
-
   :config
-  (custom-set-faces
-   ;; Make classNames italic
-   '(rjsx-attr ((t (:inherit font-lock-variable-name-face
-                             :slant italic :height 1.15 :family "Inconsolata")))))
   (defun my-rjsx-mode-hook ()
-    (prettier-js-mode)
-    (auto-complete-mode))
+    (custom-set-faces
+     ;; Make classNames italic
+     '(rjsx-attr ((t (:inherit font-lock-variable-name-face
+                               :slant italic :height 1.15 :family "Inconsolata"))))))
   (add-hook 'rjsx-mode-hook 'my-rjsx-mode-hook))
 
 (provide 'rjsx-config)
