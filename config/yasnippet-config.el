ccabf1f4d547279701b3b0153a24b4e56ac15f3b
Tejas Bubane <tejasbubane@gmail.com>
2017-11-16 13:34:40 +0530

    Fix yasnippet loadpath and add a snippet for jsx mode
    import React snippet - more to be added.

--- config/yasnippet-config.el
+++ config/yasnippet-config.el
@@ -1,6 +1,6 @@
 (use-package yasnippet
   :config
-  (setq yas-snippet-dirs '("snippets/"))
+  (setq yas-snippet-dirs (concat user-emacs-directory (convert-standard-filename "snippets/")))
   (yas-global-mode 1))
 
 (provide 'yasnippet-config)
