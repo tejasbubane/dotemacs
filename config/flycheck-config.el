6db963afcc6f23a4ed7b2fa5838de7d34feca45c
Tejas Bubane <tejasbubane@gmail.com>
2018-02-20 15:29:06 +0530

    Disable checkdoc flycheck for emacs-lisp buffers

--- config/flycheck-config.el
+++ config/flycheck-config.el
@@ -3,6 +3,8 @@
 
   :config
   (advice-add 'flycheck-eslint-config-exists-p :override (lambda() t))
-  (global-flycheck-mode))
+  (global-flycheck-mode)
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda () (add-to-list 'flycheck-disabled-checkers 'emacs-lisp-checkdoc))))
 
 (provide 'flycheck-config)
