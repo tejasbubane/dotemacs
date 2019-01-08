1de21d13d9eed684959dfe5512b5f204b9cd4d7c
Tejas Bubane <tejasbubane@gmail.com>
2019-01-08 14:16:56 +0530

    Show battery status in modeline
    Using `fancy-battery`: https://github.com/lunaryorn/fancy-battery.el

--- config/spaceline-mode-config.el
+++ config/spaceline-mode-config.el
@@ -1,4 +1,9 @@
 (use-package spaceline
+  :init
+  (use-package fancy-battery
+  :config
+  (fancy-battery-mode))
+
   :config
   (require 'spaceline-config)
   (setq powerline-default-separator (quote arrow))
