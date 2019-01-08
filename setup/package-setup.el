2bf18830256fd4c70e1d490ed13ab3b3f2fb4a0d
Tejas Bubane <tejasbubane@gmail.com>
2019-01-08 13:32:22 +0530

    Make package update automatically
    Using https://github.com/rranelli/auto-package-update.el

--- setup/package-setup.el
+++ setup/package-setup.el
@@ -7,21 +7,34 @@
 (setq package--init-file-ensured t)
 (package-initialize)
 
+;; use-package config
+;; putting it here and not in config file
+;; because this needs to be loaded before all packages
+;; https://github.com/jwiegley/use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   ;; Install use-package itself using package.el
   (package-install 'use-package))
-  ;; (add-to-list 'package-selected-packages 'use-package)
 
 (eval-when-compile
   (require 'use-package))
-
 ;; Install packages if not available
 ;; No need to give `require t` for each package
 (setq use-package-always-ensure t)
+;; use :diminish from use-package macro
+(use-package diminish)
+;; use any :bind variant
+(require 'bind-key)
 
-(use-package diminish) ;; to use :diminish from use-package macro
-
-(require 'bind-key) ;; to use any :bind variant
+;; auto-package-update config
+;; https://github.com/rranelli/auto-package-update.el
+;; Taken from https://www.youtube.com/watch?v=2-5kQ7KGQno
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-interval 4) ;; update every 4 days
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-prompt-before-update t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 (provide 'package-setup)
