3d0351d5f6eecf7a8750e2fe91465560784005bd
Tejas Bubane <tejasbubane@gmail.com>
2019-01-08 14:02:10 +0530

    Use spaceline instead of powerline
    Config taken from this video: https://www.youtube.com/watch?v=XUoiyqjSors&t=4s

--- config/powerline-config.el
+++
@@ -1,6 +0,0 @@
-(use-package powerline
-  :config
-  (setq powerline-image-apple-rgb t)
-  (powerline-default-theme))
-
-(provide 'powerline-config)
---
+++ config/spaceline-mode-config.el
@@ -0,0 +1,7 @@
+(use-package spaceline
+  :config
+  (require 'spaceline-config)
+  (setq powerline-default-separator (quote arrow))
+  (spaceline-spacemacs-theme))
+
+(provide 'spaceline-mode-config)
