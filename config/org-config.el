03198503084256deaadabf60748f1b4d4d4e3ad4
Tejas Bubane <tejasbubane@gmail.com>
2019-04-15 12:10:24 +0530

    Add use-package for org-bullets

--- config/org-config.el
+++ config/org-config.el
@@ -1,6 +1,8 @@
 (require 'org)
 
 ;; Use org-bullets for pretty bullets in org buffers
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (provide 'org-config)
