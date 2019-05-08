3b3fca58c57f37f7aa2bb6e7fef0ca306a724b86
Tejas Bubane <tejasbubane@gmail.com>
2019-05-08 14:13:11 +0530

    Add command to kill all magit buffers
    Ref: http://manuel-uberti.github.io/emacs/2018/02/17/magit-bury-buffer/

--- config/magit-config.el
+++ config/magit-config.el
@@ -6,6 +6,13 @@
   (setq magit-repository-directories
         '(("~/Work" . 2)
           ("~/opensource" . 1)
-          ("~/exercism_submissions" . 1))))
+          ("~/exercism_submissions" . 1)))
+  (defun mu-magit-kill-buffers ()
+    "Restore window configuration and kill all Magit buffers."
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
+  (bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map))
 
 (provide 'magit-config)
