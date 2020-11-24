cbf41fff3ef8c49682d368a7f83f54918963724c
Tejas Bubane <tejasbubane@gmail.com>
2020-11-25 03:14:36 +0530

    Move prolog.el to vendor
    vendor will have all third-party files (that are not on melpa) and
    utils will have utility functions.

--- config/prolog-mode-config.el
+++ config/prolog-mode-config.el
@@ -1,5 +1,6 @@
 ;; Taken from https://bruda.ca/emacs/prolog_mode_for_emacs
 
+(require 'prolog)
 (autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
 (autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
