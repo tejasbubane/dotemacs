c084dec254a1aa48d10db617b83105d4a4ae7b24
Tejas Bubane <tejasbubane@gmail.com>
2021-02-10 00:06:58 +0530

    Remove intero-mode from haskell config
    Project has reached EOL and is removed from melpa.

--- config/haskell-config.el
+++ config/haskell-config.el
@@ -1,10 +1,6 @@
-(use-package intero
-  :commands intero-mode)
-
 (use-package haskell-mode
   :commands haskell-mode
   :config
-  ;; (add-hook 'haskell-mode-hook 'intero-mode)
   (add-hook 'haskell-mode-hook 'fira-code-mode))
 
 (provide 'haskell-config)
