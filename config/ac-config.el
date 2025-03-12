10b7f009b830556e2a1a698449326a4dd1a3a081
Tejas Bubane <tejasbubane@gmail.com>
2025-03-12 19:49:18 +0400

    Fix autocomplete mode issue with emacs 30

--- config/ac-config.el
+++ config/ac-config.el
@@ -3,6 +3,13 @@
   :config
   (ac-config-default)
   (setq ac-ignore-case nil)
-  (global-auto-complete-mode))
+  (global-auto-complete-mode)
+  ;; ac-mode has issues with emacs 30
+  ;; Below is a workaround to fix that issue
+  ;; https://github.com/auto-complete/auto-complete/issues/533
+  ;; Remove if irrelevant in future
+  (add-hook 'auto-complete-mode-hook
+          (lambda ()
+            (setq ac-sources (remove 'ac-source-abbrev ac-sources)))))
 
 (provide 'ac-config)
